7644e38102116ee0d4765698860b7c9cbc887292
Mahmoud-kh1 <hhh116716@gmail.com>
2025-01-22 19:03:43 +0200

    commit add and file in blobs becomes files only

--- proj2/gitlet/Blobs.java
+++ proj2/gitlet/Blobs.java
@@ -3,16 +3,16 @@ package gitlet;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class Blobs {
 
   public static void create(File blob, String sha1) throws IOException {
-      File folderToBolb = new File(Repository.BLOBS_DIR, sha1);
-      if (folderToBolb.exists()){
+      File newBlob = new File(Repository.BLOBS_DIR, sha1);
+      if (newBlob.exists()) {
           return;
       }
-      folderToBolb.mkdir();
-      File file = new File(folderToBolb, blob.getName());
-      Files.copy(blob.toPath(), file.toPath());
+      newBlob.createNewFile();
+      Files.copy(blob.toPath(), newBlob.toPath(), StandardCopyOption.REPLACE_EXISTING);
   }
 }
